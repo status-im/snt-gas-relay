d45564d5db340621c3a5d14792ecc7c233bb6484
Richard Ramos <info@richardramos.me>
2018-08-18 12:31:35 -0400

    Verifying node balance

--- gas-relayer/src/service.js
+++ gas-relayer/src/service.js
@@ -28,73 +28,87 @@ events.on('web3:connected', connURL => {
   settings.process();
 });
 
-
 // Setting up Whisper options
 const shhOptions = {
   ttl: config.node.whisper.ttl,
   minPow: config.node.whisper.minPow
 };
 
-events.on('setup:complete', (settings) => {
-  let symKId;
+const verifyBalance = async (exitSubs) => {
+  const nodeBalance = await web3.eth.getBalance(config.node.blockchain.account);
+  if(web3.utils.toBN(nodeBalance).lte(web3.utils.toBN(100000))){ // TODO: tune minimum amount required for transactions
+    console.log("Not enough balance available for processing transactions");
+    console.log("> Account: %s", config.node.blockchain.account);
+    console.log("> Balance: %s", nodeBalance);
+
+    if(exitSubs){
+      web3.shh.clearSubscriptions();
+    }
+
+    process.exit(0);
+  }
+};
+
+events.on('exit', () => {
+  web3.shh.clearSubscriptions();
+  console.log("Closing service...");
+  process.exit(0);
+});
+
+events.on('setup:complete', async (settings) => {
+  // Verifying relayer balance
+  await verifyBalance();
+
+  shhOptions.symKeyId = await web3.shh.addSymKey(config.node.whisper.symKey);
+  shhOptions.kId = await web3.shh.newKeyPair();
 
   // Listening to whisper
-  web3.shh.addSymKey(config.node.whisper.symKey)
-  .then(symKeyId => { 
-      symKId = symKeyId;
-      return web3.shh.newKeyPair();
-    })
-  .then(keyId => {
-    shhOptions.symKeyId = symKId;
-    shhOptions.kId = keyId;
+  console.info(`Sym Key: ${config.node.whisper.symKey}`);
+  console.info("Topics Available:");
+  for(let contract in settings.contracts) {
+    console.info("- %s: %s [%s]", settings.getContractByTopic(contract).name, contract,  Object.keys(settings.getContractByTopic(contract).allowedFunctions).join(', '));
+    shhOptions.topics = [contract];
+    events.emit('server:listen', shhOptions, settings);
+  }
 
-    console.info(`Sym Key: ${config.node.whisper.symKey}`);
-    console.info("Topics Available:");
-    for(let contract in settings.contracts) {
-      console.info("- %s: %s [%s]", settings.getContractByTopic(contract).name, contract,  Object.keys(settings.getContractByTopic(contract).allowedFunctions).join(', '));
-      shhOptions.topics = [contract];
-      events.emit('server:listen', shhOptions, settings);
-    }
+  /*
+  if(config.heartbeat.enabled){
 
-    if(config.heartbeat.enabled){
+    web3.shh.addSymKey(config.heartbeat.symKey)
+      .then(heartbeatSymKeyId => { 
 
-      web3.shh.addSymKey(config.heartbeat.symKey)
-        .then(heartbeatSymKeyId => { 
+        for(let tokenAddress in settings.getTokens()){
 
-          for(let tokenAddress in settings.getTokens()){
+          let heartbeatPayload = settings.getToken(tokenAddress);
+          heartbeatPayload.address = tokenAddress;
 
-            let heartbeatPayload = settings.getToken(tokenAddress);
-            heartbeatPayload.address = tokenAddress;
+          setInterval(() => {
+              web3.shh.post({ 
+                symKeyID: heartbeatSymKeyId, 
+                sig: keyId,
+                ttl: config.node.whisper.ttl, 
+                powTarget:config.node.whisper.minPow, 
+                powTime: config.node.whisper.powTime,
+                topic: web3.utils.toHex("relay-heartbeat-" + heartbeatPayload.symbol).slice(0, 10),
+                payload: web3.utils.toHex(JSON.stringify(heartbeatPayload))
+            }).catch((err) => {
+              console.error(err);
+              process.exit(-1);
+            });
+          }, 60000);
 
-            setInterval(() => {
-                web3.shh.post({ 
-                  symKeyID: heartbeatSymKeyId, 
-                  sig: keyId,
-                  ttl: config.node.whisper.ttl, 
-                  powTarget:config.node.whisper.minPow, 
-                  powTime: config.node.whisper.powTime,
-                  topic: web3.utils.toHex("relay-heartbeat-" + heartbeatPayload.symbol).slice(0, 10),
-                  payload: web3.utils.toHex(JSON.stringify(heartbeatPayload))
-              }).catch((err) => {
-                console.error(err);
-                process.exit(-1);
-              });
-            }, 60000);
-
-          }
-      });
-    }
-
-
-  });
+        }
+    });
+  }*/
 });
-
 
 events.on('server:listen', (shhOptions, settings) => {
-  let processor = new MessageProcessor(config, settings, web3, shhOptions.kId);
-  web3.shh.subscribe('messages', shhOptions, (error, message) => processor.process(error, message));
+  let processor = new MessageProcessor(config, settings, web3, shhOptions.kId, events);
+  web3.shh.subscribe('messages', shhOptions, (error, message) => {
+    verifyBalance(true);
+    processor.process(error, message);
+  });
 });
-
 
 // Daemon helper functions
 
