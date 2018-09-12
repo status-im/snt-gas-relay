34963e403f9070f96d04e5eb531c702ff1159188
Richard Ramos <info@richardramos.me>
2018-09-12 11:50:32 -0400

    Added testnet configuration for gas relayer service

---
+++ gas-relayer/config/config.testnet.js
@@ -0,0 +1,78 @@
+module.exports = {
+    "gasPrice": 20,
+    "node": {
+        "local":{
+            "protocol": "ws",
+            "host": "localhost",
+            "port": 8546
+        },
+        "ganache": {
+            "protocol": "http",
+            "host": "localhost",
+            "port": 8545
+        },
+        "blockchain": {
+            "account": "d13c733f32970e5282981fa4a738682ba3c1e2d0"
+        },
+        "whisper": {
+            "symKey": "0xd0d905c1c62b810b787141430417caf2b3f54cffadb395b7bb39fdeb8f17266b",
+            "ttl": 1000,
+            "minPow": 0.2,
+            "powTime": 1000
+        }
+    },
+    
+    "tokens": {
+        "0x0000000000000000000000000000000000000000": {
+            "name": "Ethereum",
+            "symbol": "ETH",
+            "minAccepted":{
+                "value": 1,
+                "currency": "USD"
+            }
+        },
+        "0xd7879e4401c548544196dc0215df449B2a1E23B3": {
+            "name": "Status Test Token",
+            "symbol": "SNT",
+            "minAccepted":{
+                "value": 1,
+                "currency": "USD"
+            },
+            "pricePlugin": "../plugins/token-utils.js"
+        }
+    },
+
+    "contracts":{
+        "IdentityGasRelay": {
+            "abiFile": "../abi/IdentityGasRelay.json",
+            "isIdentity": true,
+            "factoryAddress": "0x7F106A1Bc637AC4AAed3DC72582749c4562D4323",
+            "kernelVerification": "isKernel(bytes32)",
+            "allowedFunctions": [
+                { 
+                    "function": "approveAndCallGasRelayed(address,address,uint256,bytes,uint256,uint256,uint256,address,bytes)", 
+                    "isToken": true
+                },
+                {
+                    "function": "callGasRelayed(address,uint256,bytes,uint256,uint256,uint256,address,bytes)",
+                    "isToken": false
+                }
+            ],
+            "strategy": "../src/strategy/IdentityStrategy.js"
+        },
+        "SNTController": {
+            "abiFile": "../abi/SNTController.json",
+            "isIdentity": false,
+            "address": "0xA77A1014F55157c3119FB3f53E653E42f8fa634c",
+            "allowedFunctions": [
+                {
+                    "function":"transferSNT(address,uint256,uint256,uint256,bytes)"
+                },
+                {
+                    "function":"executeGasRelayed(address,bytes,uint256,uint256,uint256,bytes)"
+                }
+            ],
+            "strategy": "../src/strategy/SNTStrategy.js"
+        }
+    }
+};
