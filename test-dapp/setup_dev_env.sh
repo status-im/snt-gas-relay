db48083dfbb7274d5ddf391be46a6cb39a9d8e01
Richard Ramos <info@richardramos.me>
2018-10-10 16:07:20 -0400

    Updating setup for dev

--- test-dapp/setup_dev_env.sh
+++ test-dapp/setup_dev_env.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-geth account import test_pk --password config/development/password --datadir .embark/development/datadir/
-geth account import test2_pk --password config/development/password --datadir .embark/development/datadir/
-
-echo 'Waiting 5s before unlocking the account'
-sleep 5s
-geth --exec "personal.unlockAccount('0xb8d851486d1c953e31a44374aca11151d49b8bb3', 'dev_password', 0)" attach .embark/development/datadir/geth.ipc
-geth --exec "personal.unlockAccount('0xacB57b896A80FCA848E279e87043d52e84f2c6Db', 'dev_password', 0)" attach .embark/development/datadir/geth.ipc
-
 STT=`cat chains.json | awk /STT/,/}/  | grep -Pzo "0x[0-9A-Za-z]+"`
 IdentityFactory=`cat chains.json | awk /IdentityFactory/,/}/  | grep -Pzo "0x[0-9A-Za-z]+"`
 SNTController=`cat chains.json | awk /SNTController/,/}/  | grep -Pzo "0x[0-9A-Za-z]+"`
