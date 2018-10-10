4ff9212dc26d39199b69ce0529c1afb9a8ba6dbb
Richard Ramos <info@richardramos.me>
2018-10-10 15:57:11 -0400

    Adding development settings

--- test-dapp/app/config.js
+++ test-dapp/app/config.js
@@ -1,5 +1,5 @@
 const config = {
-    "relayAccount": "0xb8d851486d1c953e31a44374aca11151d49b8bb3",
+    "relayAccount": "0x5b9b5db9cde96fda2e2c88e83f1b833f189e01f4",
     "relaySymKey": "0xd0d905c1c62b810b787141430417caf2b3f54cffadb395b7bb39fdeb8f17266b"
 };
 
