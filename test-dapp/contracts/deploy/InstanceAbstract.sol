398642ecf85a0d5ef7539a54105d8572aab362cd
Ricardo Guilherme Schmidt <ricardo3@status.im>
2018-12-27 16:01:38 -0200

    doc fix

--- test-dapp/contracts/deploy/InstanceAbstract.sol
+++ test-dapp/contracts/deploy/InstanceAbstract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 /**
  * @title InstanceAbstract
  * @author Ricardo Guilherme Schmidt (Status Research & Development GmbH) 
- * @dev Defines kernel vars that Kernel contract share with Instance.
+ * @dev Defines instance common storage
  *      Important to avoid overwriting wrong storage pointers is that 
  *      InstanceAbstract should be always the first contract at heritance.
  */
